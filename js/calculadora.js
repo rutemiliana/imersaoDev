1ab0c30daf2b1562f1ffb53f694d8b8b7d9ac825
Rute Miliana <rutemiliana13@gmail.com>
2022-09-06 19:54:46 -0300

    Calculadora de média. Aula 01

---
+++ js/calculadora.js
@@ -0,0 +1,7 @@
+let notaPrimeiroBimestre = 9.80;
+let notaSegundoBimestre = 8.9;
+let notaTerceiroBimestre = 6.7;
+let notaQuartoBimestre = 9.9;
+let notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+let notaFomatada = notaFinal.toFixed(2);
+console.log(notaFomatada);
